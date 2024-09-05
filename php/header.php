7f357c3c37130ffe1f0d51e8d4d85bb22f76aa12
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-05 23:19:16 +0200

    Update header.php

--- php/header.php
+++ php/header.php
@@ -15,6 +15,7 @@ $userName = $isLoggedIn ? $_SESSION['nom_utilisateur'] : ''; // Récupérer le n
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>639</title>
+    <link rel="stylesheet" href="../css/header.css">
 </head>
 <body class="head">
 
