0805302b6b6a1b6f7cd6aefb7b85ed77cf68f30b
Ist4lri <pierreloic.gouttebel@gmail.com>
2024-09-05 18:51:36 +0200

    css

--- php/profil_utilisateur.php
+++ php/profil_utilisateur.php
@@ -66,7 +66,7 @@ $excel_file_path = "../excel/planning_utilisateurs.xlsx";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil Utilisateur</title>
     <link rel="stylesheet" href="../css/profil.css">
-    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="../css/header.css">
     
 </head>
 <body>
