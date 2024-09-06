47a6c45c4acd867dfe03541c8e5c9e43952bf228
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-06 02:26:27 +0200

    Update ins.php

--- php/ins.php
+++ php/ins.php
@@ -43,7 +43,7 @@ if (isset($_SESSION['utilisateur'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/form.css">
 </head>
 <body>
 
