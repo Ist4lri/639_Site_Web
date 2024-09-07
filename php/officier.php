8d45bec8362ead4c74acbe8113b8a139edb5018e
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-07 16:46:19 +0200

    Update officier.php

--- php/officier.php
+++ php/officier.php
@@ -68,6 +68,7 @@ $specialitesStmt = $pdo->query("SELECT id, nom FROM spe");
 $specialites = $specialitesStmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+<?php include 'header.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
