248523fc99a614928f82c1bfc3fa09d4f3e375e6
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-11 16:27:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,6 @@ $userName = $isLoggedIn ? $_SESSION['nom_utilisateur'] : '';
             width: 30%;
             border-collapse: collapse;
             margin-bottom: 20px;
-            background-color: #f5f5F5;
         }
 
         table, th, td {
@@ -94,7 +93,7 @@ $userName = $isLoggedIn ? $_SESSION['nom_utilisateur'] : '';
             padding: 10px;
             text-align: center;
         }
-    
+    </style>
 </head>
 <body>
 
@@ -123,7 +122,7 @@ $userName = $isLoggedIn ? $_SESSION['nom_utilisateur'] : '';
     </nav>
 </header>
 
-        <div class="intro_content">
+<div class="intro_content">
     <?php if ($isLoggedIn): ?>
         <h1>Bienvenue dans la Garde Impériale, soldat du 639ème Régiment Cadien.</h1>
         <p>Nous espérons de vous une efficacité et une assiduité exemplaire. 
@@ -158,7 +157,8 @@ $userName = $isLoggedIn ? $_SESSION['nom_utilisateur'] : '';
                     <th>Spécialité</th>
                     <th>Places restantes</th>
                 </tr>
-                <?php foreach ($specialties as $specialty): ?>
+                <?php 
+                foreach ($specialties as $specialty): ?>
                     <?php
                 if (isset($specialty['id']) && $specialty['id'] > 8) {
                     $specialty['id'] = 8; // Cap the value to 8
@@ -189,66 +189,83 @@ $userName = $isLoggedIn ? $_SESSION['nom_utilisateur'] : '';
             </table>
         </div>
 
-
-<div class="intro_content">
-    <h1>Le 639th régiment</h1>
+    <?php else: ?>
+        <!-- Contenu pour les visiteurs non connectés -->
+        <h1>Le 639th régiment</h1>
         <h3>Une communauté A l&apos;écoute et présente.</h3>
-            <p>
-                Ici, au régiment, on est environ une soixantaine a se battre pour l&apos;empereur ! Lorsque tu nous rejoindras, tu pourras trouver des personnes présentes
-                pour te guider et répondre à toutes tes questions. Tu trouveras également des frères d&apos;armes, qui te soutiendront en tout lieu, et qui sauront te 
-                guider afin que tu passes une bonne soirée, et surtout, que tu ne sois pas perdu au milieu du champ de bataille. De plus, si jamais tu as le moindre soucis,
-                il y a une incroyable équipe de support qui seront ravis de t&apos;aider, tous dirigé par Cicéron, le chef des ressources humaines. Si tu as le moindre problème
-                avec qui que ce soit, il saura trouver une solution afin que tu puisses continuer à être dans une bonne atmosphère. Tu as également Sieger, le responsable modeur,
-                qui a la charge de gérer le mod personnel de la team, ainsi que ce qui touche a du code. Enfin tu as le grand manitou, Jager, qui est le leader de la team.
-                Il s&apos;occupe de tout ce qui est administratif, et gère le serveur administrativement et financièrement parlant.
-            </p>
+        <p>
+            Ici, au régiment, on est environ une soixantaine a se battre pour l&apos;empereur ! Lorsque tu nous rejoindras, tu pourras trouver des personnes présentes
+            pour te guider et répondre à toutes tes questions. ...
+        </p>
         <h3>Un serveur français Milsim</h3>
-            <p>
-                Nous sommes une team Française <img src="src/assets/drapeau_fr.png" alt="Drapeau Français"/> pratiquant le Milsim
-                semi-sérieux. Lors de nos opérations, nous avons toujours un temps ou nous pouvons délirer, parler, et échanger un peu, mais durant les opérations, un RP sérieux
-                est demandé.  Cependant, un fort esprit de camaraderie est présent, ce qui permettra de combattre en étant entouré d&apos;amis, et permettra de supporter les ordres
-                blasant des gradés (Ou des comissaires...). Mais tes futurs frères d&apos;armes sauront te montrer on fait pour survivre en s&apos;amusant, et en passant une bonne soirée.
-                Enfin, sache que tu n&apos;es pas obligé de connaitre toutes l&apos;histoire de Warhammer pour pouvoir nous rejoindre : A vrai dire, c&apos;est même mieux si tu ne connais pas grand
-                chose, vus qu&apos;un garde est assez ignorants des desseins qui se trouvent au dessus de lui...
-            </p>
+        <p>
+            Nous sommes une team Française <img src="src/assets/drapeau_fr.png" alt="Drapeau Français"/> pratiquant le Milsim
+            semi-sérieux. ...
+        </p>
         <h3>Comment nous rejoindre ?</h3>
-            <p>
-                Pour nous rejoindre, rien de plus simple : Il suffit que tu rejoindre notre serveur discord ! Puis, après que tu ais lu notre réglement, tu pourras passer un entretien
-                afin de savoir comment t&apos;acceuillir. Tu seras ensuite formé, même si tu ne connais rien à Arma3 : nous pouvons prendre en charge des néophytes totaux, du moment que vous
-                la volonté de servir l&apos;empereur.
-            </p>
+        <p>
+            Pour nous rejoindre, rien de plus simple : Il suffit de rejoindre notre serveur discord ! ...
+        </p>
         <h4>Alors ? prêt à nous rejoindre ?</h4>
         <h5><a href="https://discord.gg/HUwHpEZBZx" target="_blank" rel="noopener noreferrer">Enrole toi aujourd&apos;hui !</a></h5>
+    <?php endif; ?>
 </div>
 
 
 <div class="explain_container">
-  
     <h3>Nos spécialités</h3>
-<div class="specialties">
-    <div class="row">
-        <div class="specialty"><a href="php/mg.php"><img src="../src/assets/BoutonMGunner.png" alt="Machine Gunner"></a></div>
-        <div class="specialty"><a href="php/at.php"><img src="../src/assets/BoutonATank.png" alt="Anti-Tank"></a></div>
-        <div class="specialty"><a href="php/med.php"><img src="../src/assets/BoutonMedicae.png" alt="Médicae"></a></div>
-        <div class="specialty"><a href="php/vox.php"><img src="../src/assets/BoutonVOperateur.png" alt="Vox Opérateur"></a></div>
+    <div class="specialties">
+        <div class="specialty">
+            <a href="php/mg.php">
+                <img src="src/assets/BoutonMGunner.png" alt="Machine Gunner">
+            </a>
+        </div>
+        <div class="specialty">
+            <a href="php/at.php">
+                <img src="src/assets/BoutonATank.png" alt="Anti-Tank">
+            </a>
+        </div>
+        <div class="specialty">
+            <a href="php/med.php">
+                <img src="src/assets/BoutonMedicae.png" alt="Médicae">
+            </a>
+        </div>
+        <div class="specialty">
+            <a href="php/vox.php">
+                <img src="src/assets/BoutonVOperateur.png" alt="Vox Opérateur">
+            </a>
+        </div>
+        <div class="specialty">
+            <a href="php/mark.php">
+                <img src="src/assets/BoutonMarksman.png" alt="Marksman">
+            </a>
+        </div>
+        <div class="specialty">
+            <a href="php/plas.php">
+                <img src="src/assets/BoutonPlasma.png" alt="Plasma">
+            </a>
+        </div>
+        <div class="specialty">
+            <a href="php/kboom.php">
+                <img src="src/assets/BoutonBreacher.png" alt="Breacher">
+            </a>
+        </div>
+        <div class="specialty">
+            <a href="php/etl.php">
+                <img src="src/assets/BoutonETLourd.png" alt="Equipier de Tir Lourd">
+            </a>
+        </div>
     </div>
-    <div class="row">
-        <div class="specialty"><a href="php/mark.php"><img src="../src/assets/BoutonMarksman.png" alt="Marksman"></a></div>
-        <div class="specialty"><a href="php/plas.php"><img src="../src/assets/BoutonPlasma.png" alt="Plasma"></a></div>
-        <div class="specialty"><a href="php/kboom.php"><img src="../src/assets/BoutonBreacher.png" alt="Breacher"></a></div>
-        <div class="specialty"><a href="php/etl.php"><img src="../src/assets/BoutonETLourd.png" alt="Equipier de Tir Lourd"></a></div>
-    </div>
+
+    <div class="eff"><a href="php/effectif.php">Nos Effectifs</a></div>
 </div>
 
-<div class="eff"><a href="php/effectif.php">Nos Effectif</a></div>
-
-        <script>
+<script>
 function toggleTable(tableId) {
     const table = document.getElementById(tableId);
     table.style.display = table.style.display === "none" ? "table" : "none";
 }
 </script>
-
 
 </body>
 </html>
