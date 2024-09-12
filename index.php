b2e757574f0fc463fbbb0b78cb96ecfc8f0b3588
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-12 21:09:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -253,8 +253,7 @@ foreach ($specialties as $specialty):
 
 
 <div class="explain_container">
-  
-    <div class="specialty"><img src="src/assets/NosSpécialités.png" alt="Nos spécialités"></div>
+  <img src="src/assets/NosSpécialités.png" alt="Nos spécialités" class="specialty-image">
 <div class="specialties">
     <div class="row">
         <div class="specialty"><a href="php/mg.php"><img src="src/assets/BoutonMGunner0.png" alt="Machine Gunner"></a></div>
