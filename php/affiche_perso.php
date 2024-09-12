a7e4b347d85a2fc92dc17a64e9b9f03a6ba03c22
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-12 17:33:05 +0200

    Update affiche_perso.php

--- php/affiche_perso.php
+++ php/affiche_perso.php
@@ -1,4 +1,3 @@
-<?php
 session_start();
 require 'db.php';
 require('../vendor/setasign/fpdf/fpdf.php');
@@ -16,17 +15,17 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute(['id' => $id]);
 $perso = $stmt->fetch(PDO::FETCH_ASSOC);
 
-
+// If no character is found
 if (!$perso) {
     die('Character not found.');
 }
 
-
+// Create a new FPDF instance
 $pdf = new FPDF();
-$pdf->AddPage();
+$pdf->AddPage();  
 
 
-$pdf->Image('../src/assets/fond.jpg', 0, 0, 210, 297);  //A4 (210mm x 297mm)
+$pdf->Image('../src/assets/fond.jpg', 0, 0, 210, 297);  
 
 
 $pdf->SetFont('Arial', 'B', 16);
@@ -38,10 +37,16 @@ $pdf->Cell(0, 10, 'Validation du Personnage', 0, 1, 'C');
 $pdf->Ln(10);
 
 
+$pdf->SetX(30);
+
+
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 10, 'Nom: ', 0, 0);
 $pdf->SetFont('Arial', '', 12);
 $pdf->Cell(100, 10, $perso['nom'], 0, 1);
+
+
+$pdf->SetX(30);
 
 
 $pdf->SetFont('Arial', 'B', 12);
@@ -49,33 +54,36 @@ $pdf->Cell(40, 10, 'Faction: ', 0, 0);
 $pdf->SetFont('Arial', '', 12);
 $pdf->Cell(100, 10, $perso['faction'], 0, 1);
 
-
 if ($perso['faction'] === 'Adeptus Mechanicus') {
-    $pdf->Image('../src/assets/mechanicus.png', 150, 20, 50); 
+    $pdf->Image('../src/assets/mechanicus.png', 75, 20, 50);  // Adjusted position to center horizontally
 }
 
 
 $pdf->Ln(10);
 
 
+$pdf->SetX(30);
+
+
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 10, 'Histoire: ', 0, 1);
 $pdf->SetFont('Arial', '', 12);
-$pdf->MultiCell(0, 10, $perso['histoire']);
+$pdf->MultiCell(150, 10, $perso['histoire']);
 
-//
+
+$pdf->SetX(30);
+
+
 $raison = !empty($perso['raison']) ? $perso['raison'] : 'Aucune raison spécifiée';
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 10, 'Raison: ', 0, 1);
 $pdf->SetFont('Arial', '', 12);
-$pdf->MultiCell(0, 10, $raison);
+$pdf->MultiCell(150, 10, $raison);
 
 
 if ($perso['validation'] === 'Accepter') {
-    $pdf->Image('../src/assets/sceau.png', 150, 240, 40);  // Adjust size and position as necessary
+    $pdf->Image('../src/assets/sceau.png', ($pdf->GetPageWidth() - 40) / 2, 240, 40); 
 }
 
-
+// Output the PDF to the browser
 $pdf->Output("I", "Validation_Personnage_{$perso['nom']}.pdf");
-
-?>
