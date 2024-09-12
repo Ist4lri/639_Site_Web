9893d18900dd18ff47b1ce5f138c4093e630c263
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-12 16:36:31 +0200

    Update perso.php

--- php/perso.php
+++ php/perso.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css"> 
+    <link rel="stylesheet" href="../css/perso.css"> 
     <title>Création de Personnage</title>
 </head>
 <body>
