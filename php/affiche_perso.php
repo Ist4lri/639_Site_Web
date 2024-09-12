20a36f0881fffe430f5ece8a3a2e23a3b4258dff
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-12 17:36:03 +0200

    Update affiche_perso.php

--- php/affiche_perso.php
+++ php/affiche_perso.php
@@ -16,12 +16,12 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute(['id' => $id]);
 $perso = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// If no character is found
+
 if (!$perso) {
     die('Character not found.');
 }
 
-// Create a new FPDF instance
+
 $pdf = new FPDF();
 $pdf->AddPage();  
 
@@ -38,22 +38,22 @@ $pdf->Cell(0, 10, 'Validation du Personnage', 0, 1, 'C');
 $pdf->Ln(10);
 
 
-$pdf->SetX(30);
+$pdf->SetX(25);
 
 
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 10, 'Nom: ', 0, 0);
 $pdf->SetFont('Arial', '', 12);
-$pdf->Cell(100, 10, $perso['nom'], 0, 1);
+$pdf->Cell(70, 10, $perso['nom'], 0, 1);
 
 
-$pdf->SetX(30);
+$pdf->SetX(25);
 
 
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 10, 'Faction: ', 0, 0);
 $pdf->SetFont('Arial', '', 12);
-$pdf->Cell(100, 10, $perso['faction'], 0, 1);
+$pdf->Cell(60, 10, $perso['faction'], 0, 1);
 
 if ($perso['faction'] === 'Adeptus Mechanicus') {
     $pdf->Image('../src/assets/mechanicus.png', 75, 20, 50);  // Adjusted position to center horizontally
@@ -63,23 +63,23 @@ if ($perso['faction'] === 'Adeptus Mechanicus') {
 $pdf->Ln(10);
 
 
-$pdf->SetX(30);
+$pdf->SetX(25);
 
 
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 10, 'Histoire: ', 0, 1);
 $pdf->SetFont('Arial', '', 12);
-$pdf->MultiCell(150, 10, $perso['histoire']);
+$pdf->MultiCell(40, 10, $perso['histoire']);
 
 
-$pdf->SetX(30);
+$pdf->SetX(25);
 
 
 $raison = !empty($perso['raison']) ? $perso['raison'] : 'Aucune raison spécifiée';
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 10, 'Raison: ', 0, 1);
 $pdf->SetFont('Arial', '', 12);
-$pdf->MultiCell(150, 10, $raison);
+$pdf->MultiCell(40, 10, $raison);
 
 
 if ($perso['validation'] === 'Accepter') {
