93b583e84184e4a4516ba6e693dd7a6b23021ab9
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-12 20:43:00 +0200

    Update afficher_info.php

--- php/afficher_info.php
+++ php/afficher_info.php
@@ -19,8 +19,8 @@ $sql = "SELECT im.*, u.nom AS nom_utilisateur FROM informations_medicales im
 $stmt = $pdo->query($sql);
 $informations = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Charger FPDF (assurez-vous que le chemin est correct)
-require('../vendor/setasign/fpdf/fpdf.php'); // Assurez-vous que ce chemin est valide
+// Charger FPDF
+require('../vendor/setasign/fpdf/fpdf.php');
 
 // Créer une instance de FPDF
 $pdf = new FPDF();
@@ -28,12 +28,18 @@ $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16);
 $pdf->Image('../src/assets/fond.jpg', 0, 0, 210, 297);
 
+// Calculer la position Y du milieu
+$page_height = $pdf->GetPageHeight();
+$y_milieu = $page_height / 2; // Calcul du milieu vertical de la page
+
 // Titre du PDF
 $pdf->Cell(0, 10, 'Informations Medicales', 0, 1, 'C');
 $pdf->Ln(10);
 
 // Définir la police pour le contenu
 $pdf->SetFont('Arial', '', 12);
+
+
 
 foreach ($informations as $info){
     $pdf->SetX(25);
@@ -46,9 +52,9 @@ foreach ($informations as $info){
     $pdf->Cell(0, 10, 'Poids: ' . $info['poids'] . ' kg', 0, 1);
     $pdf->SetX(25);
     $pdf->MultiCell(150, 10, 'Problemes medicaux: ' . $info['problemes_medicaux']);
-    $pdf->Ln(10); 
-
+    $pdf->Ln(10);
 }
-
+    $pdf->Image('../src/assets/mediace.png', 10, $y_milieu, 20); // largeur=20
+    $pdf->Image('../src/assets/mediace.png', 170, $y_milieu, 20); 
 $pdf->Output('I', 'informations_medicales.pdf');
 ?>
