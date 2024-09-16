306419bd83806de58f3f937665808f3356585c63
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-16 15:25:56 +0200

    Update affiche_perso.php

--- php/affiche_perso.php
+++ php/affiche_perso.php
@@ -21,6 +21,8 @@ $perso = $stmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
+
+require('../vendor/setasign/fpdf/fpdf.php');
 class PDF extends FPDF
 {
     function Header()
