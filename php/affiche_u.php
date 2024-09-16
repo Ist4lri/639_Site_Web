cd72c6a786110d9e06f54a9a89805795dad52171
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-16 16:24:43 +0200

    Update affiche_u.php

--- php/affiche_u.php
+++ php/affiche_u.php
@@ -27,8 +27,17 @@ class PDF extends FPDF
     {
         // Utiliser une image de fond pour le header
         $this->Image('../src/assets/fond.jpg', 0, 0, 210, 297);
-        // Ajuster la marge haute
         $this->SetY(15);
+    }
+
+    function Footer()
+    {
+        // Positionnement à 1,5 cm du bas
+        $this->SetY(-15);
+        // Police Arial italique 8
+        $this->SetFont('Arial', 'I', 8);
+        // Texte centré de pied de page
+        $this->Cell(0, 10, 'Page ' . $this->PageNo(), 0, 0, 'C');
     }
 }
 
