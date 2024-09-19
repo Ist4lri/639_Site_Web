8663ff6af9f6f3434ef45e2ac3205edd221262f6
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-19 21:10:46 +0200

    Update officio.php

--- php/officio.php
+++ php/officio.php
@@ -88,3 +88,16 @@
         </table>
     </div>
 </div>
+</body>
+
+<script>
+function toggleSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section.style.display === "none" || section.style.display === "") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
+</script>
+
