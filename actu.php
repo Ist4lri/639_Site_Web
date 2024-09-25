4a4c11ad13848c7b15b67b0584b07e5f67bf93e7
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-25 12:07:46 +0200

    Update actu.php

--- actu.php
+++ actu.php
@@ -34,11 +34,24 @@ if ($isAdmin && $_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actualité</title>
-    <link rel="stylesheet" href="css/style.css">
+<header class="head">
+    <div class="head-logo">
+        <a href="index.php">
+            <img src="src/assets/Logo.png" alt="Logo 639">
+        </a>
+            <span class="head-username">Bonjour, <?php echo htmlspecialchars($userName); ?></span>
+    </div>
+    <div class="head-logo2">
+        <img src="src/assets/TitreSite.png" alt="639 Régiment cadien"
+    </div>
+    <nav class="head-nav">
+            <a href="php/profil_utilisateur.php">Profil</a>
+            <a href="php/officier.php">Officier</a>
+            <a href="php/sous-officier.php">Sous-Officier</a>
+            <a href="php/Dec.php">Déconnexion</a>
+    </nav>
+    </div>
+</header>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -207,3 +220,168 @@ setInterval(moveCarousel, 3000); // Change image every 3 seconds
 
 </body>
 </html>
+
+
+<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <style>
+        .table-section {
+    padding: 5px;
+    width: 40%; 
+    margin: 3px auto; 
+    color: #9ed79d; 
+    text-align: center;
+}
+
+.table-section h3 {
+    cursor: pointer;
+    color: #3bd237;
+    margin-bottom: 3px;
+    font-size: 1.5em;
+    background-color: rgba(0, 0, 0, 0.6); 
+    padding: 10px;
+    border-radius: 8px; 
+    backdrop-filter: blur(5px); 
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+
+.table-section table {
+    width: 100%; 
+    border-collapse: collapse;
+    margin: 0 auto; 
+}
+
+table, th, td {
+    border: 2px solid #9ed79d; 
+    padding: 10px;
+    color: #9ed79d; 
+    background-color: #211;
+}
+
+th {
+    background-color: #222; 
+    font-weight: bold;
+}
+
+  .servo-mortis {
+    position: absolute;
+    right: 10px;
+    top: 150px;
+    width: 150px;
+    height: auto;
+    cursor: pointer;
+    animation: float 3s ease-in-out infinite;
+}
+
+        .officio {
+    position: absolute;
+    right: 20px;
+    top: 240px;
+    width: 150px;
+    height: auto;
+    cursor: pointer;
+}
+
+        .mech {
+    position: absolute;
+    right: 45px;
+    top: 320px;
+    width: 80px;
+    height: auto;
+    cursor: pointer;
+}
+
+        .map {
+    position: fixed;
+    left: 0px;
+    bottom: 0px;
+    width: 80px;
+    height: auto;
+    cursor: pointer;
+}
+
+.Tooltip {
+    position: absolute;
+    right: 125px; 
+    top: 120px;
+    background-color: #222222;
+    color: #00F529;
+    padding: 0px;
+    border-radius: 8px;
+    font-size: 14px;
+    border: 0.5px solid #00F529; 
+    opacity: 0;
+    font-weight: bold;
+    transition: opacity 0.3s ease;
+}
+
+@keyframes float {
+    0% {
+        transform: translateY(0);
+    }
+    50% {
+        transform: translateY(-20px);
+    }
+    100% {
+        transform: translateY(0);
+    }
+}
+.tooltip2 {
+    position: absolute;
+    right: 125px; 
+    top: 120px;
+    background-color: #222222;
+    color: #00F529;
+    padding: 0px;
+    border-radius: 8px;
+    font-size: 14px;
+    border: 0.5px solid #00F529; 
+    opacity: 0;
+    font-weight: bold;
+    transition: opacity 0.3s ease;
+}
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 40%;
+            top: 35%;
+            width: 20%;
+            height: 20%;
+            background-color: #424242;
+            justify-content: center;
+            align-items: center;
+            border-radius: 10px;
+            border: 2px solid #3bd237;
+        }
+
+        .modal-content {
+            position: relative; 
+            background-color: #424242;
+            color: #9ed79d;
+            padding: 20px;
+            border-radius: 5px;
+            text-align: center;
+        }
+
+        .close {
+            position: absolute;
+            top: -35px;
+            right: 5px;
+            color: red;
+            font-size: 28px;
+            font-weight: bold;
+            cursor: pointer;
+        }
+
+        .close:hover {
+            color: darkred;
+        }
+
+  
+    </style>
+</head>
+<body>
+
+
