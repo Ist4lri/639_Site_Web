960c063d43eaaf33008314ff94323e92628cf612
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-25 10:30:40 +0200

    Update campagne.php

--- php/campagne.php
+++ php/campagne.php
@@ -2,6 +2,10 @@
 session_start();
 include 'db.php'; 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $stmt = $pdo->prepare("SELECT id, nom FROM utilisateurs WHERE email = :email");
 $stmt->execute(['email' => $_SESSION['utilisateur']]);
 $currentUser = $stmt->fetch(PDO::FETCH_ASSOC);
