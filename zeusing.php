9d74e21d8072635e310399ca4ed18772f188037d
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-26 18:27:13 +0200

    Update zeusing.php

--- zeusing.php
+++ zeusing.php
@@ -48,6 +48,8 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des Zeus et Mappeurs</title>
     <link rel="stylesheet" href="css/tab.css">
+    <a href="back.php">Back</a>
+    <a href="index.php">Acceuil</a>
 </head>
 <body>
 
