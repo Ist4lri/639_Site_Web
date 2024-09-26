bf9f8ec4df942fb4ec59e442559255e1e312db52
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-26 16:16:54 +0200

    Update mechanicus.php

--- php/mechanicus.php
+++ php/mechanicus.php
@@ -57,26 +57,6 @@ if ($faction) {
     </style>
 
 
-<header>
-    <div class="head-logo2">
-        <a href="../index.php">
-            <img src="../src/assets/TitreSite.png" alt="639 Régiment cadien">
-        </a>
-    </div>
-
-    <nav class="head-nav">
-        <?php if ($faction): ?>
-            <!-- Si l'utilisateur fait partie de l'Adeptus Mechanicus -->
-            <a href="lithaniem.php">Lithanies </a>
-            <a href="demande_mechanicus.php">Demandes</a>
-        <?php else: ?>
-            <!-- Si l'utilisateur n'est pas dans l'Adeptus Mechanicus -->
-            <a href="lithaniem.php">Lithanies </a>
-            <a href="profil_utilisateur.php">Profil</a>
-            <a href="Dec.php">Déconnexion</a>
-        <?php endif; ?>
-    </nav>
-</header>
 
 <div class="container">
     <?php if ($faction): ?>
