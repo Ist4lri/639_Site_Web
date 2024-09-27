150ecf5c06ec4a39519875e2eac5b7eda3748064
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-09-27 09:59:11 +0200

    Update eglise.php

--- php/eglise.php
+++ php/eglise.php
@@ -43,7 +43,7 @@ if ($faction) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adeptus Mechanicus</title>
+    <title>Adeptus Ministorum</title>
     <link rel="stylesheet" href="../css/ecclesiarchie.css">
 </head>
 <body>
