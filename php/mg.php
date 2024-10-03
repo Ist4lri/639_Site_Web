73e47822508debb9c8f479d0a31b95d5eeeb9888
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-10-03 13:44:18 +0200

    Update mg.php

--- php/mg.php
+++ php/mg.php
@@ -57,15 +57,17 @@ $stmt = $pdo->prepare("SELECT spe_id, gerance FROM utilisateurs WHERE id = :id")
 $stmt->execute(['id' => $idUtilisateur]);
 $utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if ($utilisateur && $utilisateur['spe_id'] == 1 && in_array($utilisateur['gerance'], [1, 2])) {
-    
-    // Chemin vers le fichier vox.pdf
-    $filePath = __DIR__ . '/pdf/mg.pdf';
+$filePath = __DIR__ . '/pdf/mg.pdf';
     if (file_exists($filePath)) {
         echo "<p>Un fichier PDF MG est disponible : <a href='pdf/mg.pdf' target='_blank'>Afficher</a></p>";
     } else {
         echo "<p>Aucun fichier PDF disponible pour le moment.</p>";
     }
+
+if ($utilisateur && $utilisateur['spe_id'] == 1 && in_array($utilisateur['gerance'], [1, 2])) {
+    
+    // Chemin vers le fichier vox.pdf
+    
     
     ?>
     <h2>Upload du fichier PDF</h2>
