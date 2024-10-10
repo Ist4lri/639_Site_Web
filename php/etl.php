3f756e6b64c5e4936403d5ec516b0ae5dddaa9e1
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-10-10 21:52:23 +0200

    Update etl.php

--- php/etl.php
+++ php/etl.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Équipier de Tir Lourd - Astra Militarum</title>
+    <link rel="icon" type="image/x-icon" href="src/assets/Logo_639th_2.ico">
     <link rel="stylesheet" href="../css/spe.css"> <!-- Lien vers le fichier CSS -->
 </head>
 <body>
