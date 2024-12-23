80c53d4557ee78b9a821db8d9195f475b9596b4e
Mrplayer1009 <30444295+Mrplayer1009@users.noreply.github.com>
2024-12-23 21:32:10 +0100

    Update officier.php

--- php/officier.php
+++ php/officier.php
@@ -132,7 +132,7 @@ $userName = $isLoggedIn ? $_SESSION['nom_utilisateur'] : '';
     </div>
     <nav class="head-nav">
             <a href="profil_utilisateur.php">Profil</a>
-            <a href="officier.php">Grades</a>
+            <a href="medaille.php">Médailles</a>
             <a href="demande.php">Demandes</a>
             <a href="Dec.php">Déconnexion</a>
     </nav>
